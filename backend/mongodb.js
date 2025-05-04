9f79fb16def586693ef0ff88854f5d27a061591a
Kulani Mithma <137183245+miku2003@users.noreply.github.com>
2025-05-04 11:06:59 +0530

    Delete backend directory

--- backend/mongodb.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/arpico_online', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("✅ MongoDB Connected"))
-  .catch(err => console.error("❌ MongoDB Connection Failed:", err));
-
-module.exports = mongoose;

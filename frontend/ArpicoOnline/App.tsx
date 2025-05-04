bbec78152189c71bdc221c0cd13ac579b9d31ba4
Kulani Mithma <137183245+miku2003@users.noreply.github.com>
2025-05-04 11:07:45 +0530

    Delete frontend directory

--- frontend/ArpicoOnline/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Navigation from './src/navigation';
-import { enableScreens } from 'react-native-screens';
-
-enableScreens();  // <-- Add this line
-
-export default function App() {
-  return <Navigation />;
-}
--- frontend/ArpicoOnline/index.js
+++
@@ -1,6 +0,0 @@
-import 'react-native-gesture-handler';  // Make sure this is the first line
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
